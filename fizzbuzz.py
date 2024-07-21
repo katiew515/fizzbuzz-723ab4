379d6d33ef0f1aa31fb1d481dc234ef158f47b65
katiew515 <149527611+katiew515@users.noreply.github.com>
2024-07-21 11:44:20 -0400

    Fizzbuzz assignment

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,11 @@
 # add your code here
+for i in range (100):
+    if (i+1)%3 == 0 and (i+1)%5 == 0:
+        print ("Fizzbuzz")
+    elif (i+1)%3 == 0 and not (i+1)%5 == 0:
+        print ("Fizz")
+    elif (i+1)%5 == 0 and not (i+1)%3 == 0:
+        print ('Buzz')
+    else: 
+        print(i)
 
